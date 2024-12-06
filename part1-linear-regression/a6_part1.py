f6ead05112eca17de721f0790ca0d70f3488edd4
Leon16723 <lscurtu@cps.edu>
2024-12-06 11:29:53 -0600

    finished

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -22,3 +22,32 @@ x = x.reshape(-1,1)
 # Print out the prediction
 
 # Create the model in matplotlib and include the line of best fit
+
+data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
+x = data["Age"].values
+y = data["Blood Pressure"].values
+
+
+x = x.reshape(-1,1)
+
+model = LinearRegression()
+model.fit(x, y)
+
+slope = round(model.coef_[0], 2)
+intercept = round(model.intercept_, 2)
+r_squared = round(model.score(x, y), 2)
+
+print(f"Linear equation: y = {slope}x + {intercept}")
+print(f"R-squared value: {r_squared}")
+
+age = np.array([[43]])  
+prediction = model.predict(age)
+print(f"Predicted blood pressure for someone who is 43 years old: {prediction[0]}")
+
+plt.scatter(x, y, color="blue", label="Data points")
+plt.plot(x, model.predict(x), color="red", label="Line of best fit")
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Age vs Blood Pressure")
+plt.legend()
+plt.show()
